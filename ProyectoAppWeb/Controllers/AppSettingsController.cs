9695a145c81795f3bf2c3fcce2df693d60d2ab8b
WinTux <rusok.software@gmail.com>
2024-08-29 19:50:21 -0400

    Parametros

---
+++ ProyectoAppWeb/Controllers/AppSettingsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoAppWeb.Controllers
+{
+    public class AppSettingsController : Controller
+    {
+        private IConfiguration configuration;
+
+        public AppSettingsController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.ejemplo1 = configuration["Mensaje"];
+            ViewBag.ejemplo2 = configuration["MisConfiguraciones:Config1"];
+            ViewBag.ejemplo3 = configuration["MisConfiguraciones:Config2"];
+            ViewBag.ejemplo4 = configuration["MisConfiguraciones:Config3"];
+            ViewBag.ejemplo5 = configuration["Log:AlcanceGlobal"];
+            ViewBag.ejemplo6 = configuration["Log:Debug:LogLevel:Default"];
+            return View();
+        }
+    }
+}
---
+++ ProyectoAppWeb/Controllers/ParametrosRouteController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoAppWeb.Controllers
+{
+    public class ParametrosRouteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("ejem1/{ci}")]
+        public IActionResult Ejemplo1(int ci)
+        {
+            return View();
+        }
+    }
+}
