29ad03e1bf062b028a40817bb93d2432b5a8243b
WinTux <rusok.software@gmail.com>
2024-08-30 18:55:05 -0400

    Parámetros de route - acabado

--- ProyectoAppWeb/Controllers/ParametrosRouteController.cs
+++ ProyectoAppWeb/Controllers/ParametrosRouteController.cs
@@ -2,15 +2,27 @@
 
 namespace ProyectoAppWeb.Controllers
 {
+    [Route("ParametrosRoute")]
     public class ParametrosRouteController : Controller
     {
+        [Route("Index")]
         public IActionResult Index()
         {
             return View();
         }
-        [Route("ejem1/{ci}")]
+        [Route("Ejemplo1/{ci}")]
         public IActionResult Ejemplo1(int ci)
         {
+            ViewBag.ci = ci;
+            return View();
+        }
+
+        [Route("Ejemplo2/{basee}/{altura}")]
+        public IActionResult Ejemplo2(int basee, int altura)
+        {
+            ViewBag.basee = basee;
+            ViewBag.altura = altura;
+            ViewBag.area_triangulo = basee * altura / 2;
             return View();
         }
     }
