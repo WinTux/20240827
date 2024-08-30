c6262a7a310ca2f943ae70841c1aeb2ffda25e6b
WinTux <rusok.software@gmail.com>
2024-08-30 19:50:57 -0400

    Previo para Formularios

---
+++ ProyectoAppWeb/Models/Cuenta.cs
@@ -0,0 +1,14 @@
+﻿namespace ProyectoAppWeb.Models
+{
+    public class Cuenta
+    {
+        public int Id { get; set; }
+        public string Usuario { get; set; }
+        public string Password { get; set; }
+        public string Descripcion { get; set; }
+        public bool Disponible { get; set; }
+        public List<string> Lenguajes { get; set; }
+        public string Genero { get; set; }
+        public string Cargo { get; set; }
+    }
+}
