6160b1ff6a272d6b3516f8e2780c8f03a64a58ed
WinTux <rusok.software@gmail.com>
2024-09-03 19:52:26 -0400

    Formulario 2 (producto) inicio

---
+++ ProyectoAppWeb/Controllers/FormArchivoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoAppWeb.Models;
+
+namespace ProyectoAppWeb.Controllers
+{
+    public class FormArchivoController : Controller
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+        public FormArchivoController(IWebHostEnvironment _webHostEnvironment)
+        {
+            this._webHostEnvironment = _webHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View("Index", new Producto());
+        }
+        [HttpPost]
+        public IActionResult Guardar(Producto producto, IFormFile foto)
+        {
+            if (foto == null || foto.Length == 0)
+                return Content("Archivo no valido");
+            else
+            {
+                var ruta = Path.Combine(this._webHostEnvironment.WebRootPath,"imagenes", foto.FileName);
+                return View("Resultado");
+            }
+            
+        }
+    }
+}
---
+++ ProyectoAppWeb/Models/Producto.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoAppWeb.Models
+{
+    public class Producto
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+        public string Foto { get; set; }
+    }
+}
