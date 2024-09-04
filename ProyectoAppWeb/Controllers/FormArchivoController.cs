c0c8a34007128074486c74203b2701792909bb1e
WinTux <rusok.software@gmail.com>
2024-09-04 19:52:42 -0400

    Validaciones inicio

--- ProyectoAppWeb/Controllers/FormArchivoController.cs
+++ ProyectoAppWeb/Controllers/FormArchivoController.cs
@@ -15,16 +15,42 @@ namespace ProyectoAppWeb.Controllers
             return View("Index", new Producto());
         }
         [HttpPost]
-        public IActionResult Guardar(Producto producto, IFormFile foto)
+        public IActionResult Guardar(Producto producto, IFormFile photo)
         {
-            if (foto == null || foto.Length == 0)
+            if (photo == null || photo.Length == 0)
                 return Content("Archivo no valido");
             else
             {
-                var ruta = Path.Combine(this._webHostEnvironment.WebRootPath,"imagenes", foto.FileName);
-                return View("Resultado");
+                var ruta = Path.Combine(this._webHostEnvironment.WebRootPath,"imagenes", photo.FileName);
+                var flujo = new FileStream(ruta, FileMode.Create);
+                photo.CopyToAsync(flujo);
+                producto.Foto = photo.FileName;
             }
-            
+            ViewBag.producto = producto;
+            return View("Resultado");
+        }
+        public IActionResult Index2()
+        {
+            return View("Index2", new Producto2());
+        }
+        [HttpPost]
+        public IActionResult Guardar2(Producto2 producto, IFormFile[] fotos)
+        {
+            if (fotos == null || fotos.Length == 0)
+                return Content("Archivo(s) no valido(s)");
+            else
+            {
+                producto.Fotos = new List<string>();
+                foreach (IFormFile fotito in fotos) {
+                    var ruta = Path.Combine(this._webHostEnvironment.WebRootPath, "imagenes", fotito.FileName);
+                    var flujo = new FileStream(ruta, FileMode.Create);
+                    fotito.CopyToAsync(flujo);
+                    producto.Fotos.Add(fotito.FileName);
+                }
+                
+            }
+            ViewBag.producto = producto;
+            return View("Resultado2");
         }
     }
 }
---
+++ ProyectoAppWeb/Models/Producto2.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoAppWeb.Models
+{
+    public class Producto2
+    {
+        public string Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        
+        public double Precio { get; set; }
+        public string observaciones { get; set; }
+        public List<string> Fotos { get; set; }
+    }
+}
