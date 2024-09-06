d84097f62aaa9385de5a68c493ab457e48b5c252
WinTux <rusok.software@gmail.com>
2024-09-06 19:51:06 -0400

    Sesiones 1

---
+++ ProyectoAppWeb/Controllers/SesionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoAppWeb.Models;
+
+namespace ProyectoAppWeb.Controllers
+{
+    public class SesionController : Controller
+    {
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetInt32("edad", 23);
+            HttpContext.Session.SetString("usuario", "Pepe");
+            //HttpContext.Session.SetInt32("usuario", new Producto());
+            return View();
+        }
+    }
+}
--- ProyectoAppWeb/Program.cs
+++ ProyectoAppWeb/Program.cs
@@ -8,6 +8,7 @@ namespace ProyectoAppWeb
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddSession();
 
             var app = builder.Build();
 
@@ -18,7 +19,7 @@ namespace ProyectoAppWeb
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseSession();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
