ba82000a6a159fd95ec1cb6e9d8b3bd29e0416fa
WinTux <rusok.software@gmail.com>
2024-09-13 19:24:43 -0400

    Código QR ZXing

--- ProyectoAppWeb/Tags/MiEtiquetaQRTagHelper.cs
+++ ProyectoAppWeb/Tags/MiEtiquetaQRTagHelper.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 
 namespace ProyectoAppWeb.Tags
 {
@@ -18,7 +21,27 @@ namespace ProyectoAppWeb.Tags
                     Margin = 0
                 }
             };
-            // Genera la imagen (datos) ... middleware // EF
+            var datosPixeles = codigoBarrasDatosPixeles.Write(contenido);
+
+            using (var bitmap = new Bitmap(datosPixeles.Width, datosPixeles.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb)) {
+                using (var memoriaStream = new MemoryStream()) {
+                    var datosBitmap = bitmap.LockBits(new Rectangle(0,0, datosPixeles.Width, datosPixeles.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+                    try
+                    {
+                        Marshal.Copy(datosPixeles.Pixels, 0, datosBitmap.Scan0, datosPixeles.Pixels.Length);
+                    }
+                    finally { 
+                        bitmap.UnlockBits(datosBitmap);
+                    }
+                    bitmap.Save(memoriaStream,ImageFormat.Png);
+                    output.TagName = "img"; // <img >
+                    output.Attributes.Clear();
+                    output.Attributes.Add("width", ancho); // <img width="200">
+                    output.Attributes.Add("height", alto); // <img width="200" height="200">
+                    output.Attributes.Add("src", String.Format("data:image/png;base64,{0}", Convert.ToBase64String(memoriaStream.ToArray()))); // <img width="200">
+                }
+            
+            }
         }
     }
 }
