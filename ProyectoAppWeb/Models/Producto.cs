4e7cf6f047e6532769da13fb5e838043df9dd183
WinTux <rusok.software@gmail.com>
2024-09-13 20:01:37 -0400

    Introducción a EF

--- ProyectoAppWeb/Models/Producto.cs
+++ ProyectoAppWeb/Models/Producto.cs
@@ -1,10 +1,16 @@
-﻿namespace ProyectoAppWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoAppWeb.Models
 {
+    [Table("Producto")]
     public class Producto
     {
+        [Key]
         public string Id { get; set; }
         public string Nombre { get; set; }
         public double Precio { get; set; }
+        [Column("Foto")]
         public string Foto { get; set; }
     }
 }
---
+++ ProyectoAppWeb/Models/ShoppingContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoAppWeb.Models
+{
+    public class ShoppingContext : DbContext
+    {
+        public virtual DbSet<Producto> Productos { get; set; }
+        public ShoppingContext()
+        {
+            
+        }
+        public ShoppingContext(DbContextOptions<ShoppingContext> op) : base(op) {
+        
+        }
+        
+    }
+}
